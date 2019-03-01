9ca24366f83006b646286c07c6fe17e66c1a4e93
Ravindra <ravindra@kcreativ.com>
2019-03-01 14:14:43 +0530

    code pushed

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function() {
+  // whenever we hover over a menu item that has a submenu
+  $('li.parent').on('mouseover', function() {
+    var $menuItem = $(this),
+        $submenuWrapper = $('> .wrapper', $menuItem);
+    
+    // grab the menu item's position relative to its positioned parent
+    var menuItemPos = $menuItem.position();
+    
+    // place the submenu in the correct position relevant to the menu item
+    $submenuWrapper.css({
+      top: menuItemPos.top,
+      left: menuItemPos.left + Math.round($menuItem.outerWidth() * 0.75)
+    });
+  });
+});
